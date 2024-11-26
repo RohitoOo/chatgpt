2a9518e045e104ab6c62e81a072a4ff62ca4e6ab
RohitoOo <bhambhani.ba@gmail.com>
2024-11-25 20:18:52 -0500

    Added Chat History for Continuous Conversation

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
-import { Configuration, OpenAIApi } from "openai";
-import dotenv from "dotenv";
-dotenv.config();
+import openai from "./config/open-ai.js";
+import readlineSync from "realline-sync";
+import colors from "colors";
+// async function main() {
+//   const chatCompletion = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [{ role: "user", content: "What is the capital of Florida" }],
+//   });
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
+//   console.log("Response", chatCompletion.data.choices[0]);
+// }
 
 async function main() {
-  const chatCompletion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "What is the capital of Florida" }],
-  });
+  // const userName = readlineSync.question("May I have your name?");
+  // console.log(`Hello ${userName}`);
+  console.log(colors.bold.green("Welcome to the CHATBOT Program!"));
+  console.log(colors.bold.green("You can start chatting with the bot"));
 
-  console.log("Response", chatCompletion.data.choices[0]);
+  // Store Conversation History for continuous conversation
+
+  const chatHistory = [];
+
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow("You: "));
+
+    // Constructions message by looping over history
+    try {
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      // Add Latest user's input
+
+      messages.push({
+        role: "user",
+        content: userInput,
+      });
+
+      // Call API with User's Input
+
+      const completion = await openai.creaateChatCompletion({
+        mode: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      // GET completion text
+
+      const completionText = completion.data.choices[0].message.content;
+
+      if (userInput.toLowerCase() === "exit") {
+        return;
+      }
+
+      // CHAT BOT = GREEN && USER = YELLOW
+
+      console.log(colors.green("Bot : ") + completionText);
+
+      // Update history with User's Input and ChatBot's Response
+
+      chatHistory.push(["user", userInput]);
+      chatHistory.push(["assistant", completionText]);
+    } catch (error) {
+      console.error(colors.red(error));
+    }
+  }
 }
 
 main();
